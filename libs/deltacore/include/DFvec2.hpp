42d9f52296c126c9bbf41ed5728806cf0abedb7a
YusahO <ziraelka1251@gmail.com>
2023-04-11 23:48:02 +0300

    replaced auto in operator with explicit type name

--- libs/deltacore/include/DFvec2.hpp
+++ libs/deltacore/include/DFvec2.hpp
@@ -6,11 +6,11 @@ struct DFvec2
     T x, y;
     DFvec2(): x(0), y(0) {}
     DFvec2(T x_, T y_): x(x_), y(y_) {}
-    auto operator+(const DFvec2<T> &other)
+    DFvec2<T>& operator+(const DFvec2<T> &other)
     {
         return DFvec2<T>(x + other.x, y + other.y);
     }
-    auto operator-(const DFvec2<T> &other)
+    DFvec2<T>& operator-(const DFvec2<T> &other)
     {
         return DFvec2<T>(x - other.x, y - other.y);
     }
