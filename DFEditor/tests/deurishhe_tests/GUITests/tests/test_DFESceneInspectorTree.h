10b6ba5190df7d8ae25edcbbc5d2cc38f5df59cd
Deurishhe <deurishhe@yandex.ru>
2023-05-04 17:51:36 +0300

    Added 4 hierarchy tests.

--- DFEditor/tests/deurishhe_tests/GUITests/tests/test_DFESceneInspectorTree.h
+++ DFEditor/tests/deurishhe_tests/GUITests/tests/test_DFESceneInspectorTree.h
@@ -102,4 +102,29 @@ private slots:
 
         delete scene_tree;
     }
+
+    void testMakeHierarchyFourLevels()
+    {
+        DFESceneInspectorTree *scene_tree = new DFESceneInspectorTree();
+
+        scene_tree->AddObject("Item");
+        scene_tree->AddChildObject("Item", "Item child");
+        scene_tree->AddChildObject("Item child", "Item child child 1");
+        scene_tree->AddChildObject("Item child", "Item child child 2");
+        scene_tree->AddChildObject("Item child child 1", "Item child child child");
+
+        QCOMPARE(scene_tree->SearchObjectRec("Item")->text(), "Item");
+        QCOMPARE(scene_tree->SearchObjectRec("Item child")->text(), "Item child");
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child 1")->text(), "Item child child 1");
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child 2")->text(), "Item child child 2");
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child child")->text(), "Item child child child");
+
+        QCOMPARE(scene_tree->SearchObjectRec("Item")->parent(), nullptr);
+        QCOMPARE(scene_tree->SearchObjectRec("Item child")->parent(), scene_tree->SearchObjectRec("Item"));
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child 1")->parent(), scene_tree->SearchObjectRec("Item child"));
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child 2")->parent(), scene_tree->SearchObjectRec("Item child"));
+        QCOMPARE(scene_tree->SearchObjectRec("Item child child child")->parent(), scene_tree->SearchObjectRec("Item child child 1"));
+
+        delete scene_tree;
+    }
 };
