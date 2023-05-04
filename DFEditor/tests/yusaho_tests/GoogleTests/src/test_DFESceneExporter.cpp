19713dae2985dc4b51d6264b666fcfa1a7d32a01
YusahO <ziraelka1251@gmail.com>
2023-05-04 14:53:11 +0300

    ideal testing structure

--- DFEditor/tests/yusaho_tests/GoogleTests/src/test_DFESceneExporter.cpp
+++ DFEditor/tests/yusaho_tests/GoogleTests/src/test_DFESceneExporter.cpp
@@ -1,8 +1,27 @@
 #include <gtest/gtest.h>
 
-#include ""
+// #include "DFESceneExporter.hpp"
 
-TEST(testExport)
+TEST(CollectData, test_CollectData)
 {
+}
 
+TEST(CollectData, test_nothingToCollect)
+{
+}
+
+TEST(ConvertComponent, test_ConvertComponent)
+{
+}
+
+TEST(ConvertComponent, test_nothingToConvert)
+{
+}
+
+TEST(AssembleComponents, test_AssembleComponents)
+{
+}
+
+TEST(ConvertComponent, test_nothingToAssemble)
+{
 }
